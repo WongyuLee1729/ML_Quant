accd4888f0cf74417f0cc730cbda140b96034b33
Wongyu Lee <48536472+WongyuLee1729@users.noreply.github.com>
2022-11-21 11:23:12 +0900

    Update stock_preprop.py

--- stock_preprop.py
+++ stock_preprop.py
@@ -7,18 +7,18 @@ class stock_preprop:
     makes 7:3 ratio train & test set
     time-series testing dataset
       '''
-    def __init__(self, df:pd.DataFrame, x_w, y_w, stride):
+    def __init__(self, df:pd.DataFrame, row_w, col_w, stride):
         self.df = df
-        self.x_w = x_w
-        self.y_w = y_w
+        self.row_w = row_w
+        self.col_w = col_w
         self.stride = stride
         self.ratio = np.ceil(len(self.df)/(len(self.df)*3/10))
         # self.tickers = df.columns
 
-    def XY(self):
-        x_train = np.empty((0, self.x_w))
+    def RowCol(self):
+        x_train = np.empty((0, self.row_w))
         y_train = np.empty((0, 12)) # (0,1)
-        x_test = np.empty((0, self.x_w))
+        x_test = np.empty((0, self.row_w))
         y_test = np.empty((0, 12)) # (0,1)
         cnt = 0
         
@@ -26,8 +26,8 @@ class stock_preprop:
             cnt += 1
             data = value.values
             x_start = 0
-            x_end = self.x_w
-            tmp = np.empty((0, self.x_w))
+            x_end = self.row_w
+            tmp = np.empty((0, self.row_w))
             while True:
                 if x_end >= len(data):
                     break; # tmp.size = (13,30) -> 390
@@ -38,9 +38,9 @@ class stock_preprop:
             
             if cnt%self.ratio == 0:
                 x_test = np.vstack((x_test, tmp[:-1,:]))
-                cum_rtn = ((1+tmp[1:,:self.y_w]).cumprod(axis=1)-1)[:,-1]
-                cum_rtn =cum_rtn.reshape(1,len(cum_rtn)) # tmp[1:,:self.y_w].std(1)
-                # std_rtn = tmp[1:,:self.y_w].std(1)
+                cum_rtn = ((1+tmp[1:,:self.col_w]).cumprod(axis=1)-1)[:,-1]
+                cum_rtn =cum_rtn.reshape(1,len(cum_rtn)) # tmp[1:,:self.col_w].std(1)
+                # std_rtn = tmp[1:,:self.col_w].std(1)
                 # y_test = np.vstack((y_test, np.vstack((cum_rtn, std_rtn)).T))
                 rtn = np.triu(np.ones((cum_rtn.shape[1],cum_rtn.shape[1])),0)*np.vstack([cum_rtn]*cum_rtn.shape[1])
                 y_test = np.vstack((y_test, rtn))
